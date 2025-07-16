cde3fd6e7bd373e047825d8e957822a0a2f3640d
MegaZann <32149044+MegaZann@users.noreply.github.com>
2025-07-16 05:19:01 +0300

    Update meta.user.js 1.5.6
    Обновлены заметки. 
    
    Теперь текст сохраняется. 
    Можно закрепить и изменить ширину заметок.
    Все настройки и текст сохраняется.
    
    Добавлена возможность сбросить данные заметок в настройки.
    
    Signed-off-by: MegaZann <32149044+MegaZann@users.noreply.github.com>

--- WBHelper/meta.user.js
+++ WBHelper/meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Помогатор
 // @namespace    http://tampermonkey.net/
-// @version      1.5.5
+// @version      1.5.6
 // @description  Помогатор сотрудникам службы поддержки ВБ
 // @author       Anonym
 // @match        https://portal-cc.wildberries.ru/*
