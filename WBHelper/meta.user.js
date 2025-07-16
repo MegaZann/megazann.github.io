3d284c4ff022d603c5e4bdcffe20992e9c49c840
MegaZann <32149044+MegaZann@users.noreply.github.com>
2025-07-16 06:05:50 +0300

    Update meta.user.js 1.5.8
    Исправлены мелкие ошибки.
    
    Signed-off-by: MegaZann <32149044+MegaZann@users.noreply.github.com>

--- WBHelper/meta.user.js
+++ WBHelper/meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Помогатор
 // @namespace    http://tampermonkey.net/
-// @version      1.5.7
+// @version      1.5.8
 // @description  Помогатор сотрудникам службы поддержки ВБ
 // @author       Anonym
 // @match        https://portal-cc.wildberries.ru/*
